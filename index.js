56f450e4126c125f11b3d097d35f048cd798a4a5
Brian <130288605+brian3154@users.noreply.github.com>
2026-02-25 22:01:17 +0900

    Refactor Discord bot with web server integration

--- index.js
+++ index.js
@@ -1,42 +1,53 @@
+process.on("unhandledRejection", console.error);
+process.on("uncaughtException", console.error);
+
+console.log("TOKEN exists?", !!process.env.TOKEN);
+
+process.on("unhandledRejection", console.error);
+process.on("uncaughtException", console.error);
 // 1. 주요 클래스 가져오기
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 // const { TOKEN } = require('./config.json');
 
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot is running!');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Web server running');
+});
+
 // 2. 클라이언트 객체 생성 (Guilds관련, 메시지관련 인텐트 추가)
 const client = new Client({ intents: [
-    GatewayIntentBits.Guilds, 
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers
-]});
-
-// 3. 봇이 준비됐을때 한번만(once) 표시할 메시지
-client.once(Events.ClientReady, readyClient => {
-console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+GatewayIntentBits.Guilds, 
+@@ -34,10 +16,10 @@ console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
 // TODO: 서버 인원 역할 ID를 여기에 넣으세요
+const TARGET_ROLE_ID = "1476185594446090250"; // 예: "123456789012345678"
 const TARGET_ROLE_ID = "1476113191389102301"; // 예: "123456789012345678"
 
 // TODO: 메시지를 보낼 채널 ID를 여기에 넣으세요
+const CHANNEL_ID = "1476185526829711519"; // 예: "987654321098765432"
 const CHANNEL_ID = "1476108273907142679"; // 예: "987654321098765432"
 
 client.on("guildMemberUpdate", (oldMember, newMember) => {
-  const hadRole = oldMember.roles.cache.has(TARGET_ROLE_ID);
-  const hasRole = newMember.roles.cache.has(TARGET_ROLE_ID);
-
-  // 역할이 새로 추가된 경우
-  if (!hadRole && hasRole) {
-    const channel = newMember.guild.channels.cache.get(CHANNEL_ID);
-    if (!channel) return;
-
-    // TODO: 보낼 메시지를 작성하세요
-    channel.send(`${newMember}**님 연합연맹에 오신것을 환영합니다~!**
-- <#1410296457541714040> 에서 기본적인 안내를 확인하시고
-- <#1318956381189181572> <#1339127774643748944> <#1355813336708677673> 가 기본채널입니다~
-### **여기에서 자유롭게 활동해보세요~!**`); 
-  }
+const hadRole = oldMember.roles.cache.has(TARGET_ROLE_ID);
+@@ -57,13 +39,4 @@ client.on("guildMemberUpdate", (oldMember, newMember) => {
 });
 
 // 5. 시크릿키(토큰)을 통해 봇 로그인 실행
+console.log("Trying to login...");
+
+client.login(process.env.TOKEN)
+  .then(() => console.log("Discord login success"))
+  .catch(err => console.error("Login failed:", err));
+
+client.on("ready", () => {
+  console.log("READY EVENT FIRED");
+});
+
 client.login(process.env.TOKEN);
